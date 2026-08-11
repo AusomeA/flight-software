3c2111738795f95b1bd46ae1950f4482170fc7b4
Ausome <ATAnderson@Alumni.Fullsail.Edu>
2026-08-10 20:49:00 -0600

    Update main.cpp
    New qt ui setup

--- spacecraft_simulator/main.cpp
+++ spacecraft_simulator/main.cpp
@@ -9,26 +9,10 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
+
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    SpacecraftSimulator simulator;
-    simulator.Start();
-
-    engine.rootContext()->setContextProperty("readouts", simulator.Readouts());
-    /*
-    QString log;
-
-    log += "Initial State:\n" + simulator.StateText() + "\n\n\n";
-    
-    log += "Updating simulation by 5 seconds...\n\n";
-
-    simulator.Update(5.0);
-
-    log += "State after update:\n" + simulator.StateText()+ "\n\n\n";
-    
-    engine.rootContext()->setContextProperty("logText", log);
-    */
     engine.loadFromModule("SpacecraftSimulator", "Main");
 
     return app.exec();
