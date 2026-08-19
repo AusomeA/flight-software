cf4d4e27c00686c2707b60cd538d4b583588a59f
Ausome <ATAnderson@Alumni.Fullsail.Edu>
2026-08-19 11:39:18 -0600

    main for flight computer created, cmake added
    -new main.cpp for the flight computer created
    -cmake for flight computer added (same cmake file)
    -new main.qml file

--- apps/flight_computer/main.cpp
+++ apps/flight_computer/main.cpp
@@ -1,14 +1,14 @@
 #include <QQmlApplicationEngine>
 #include <QGuiApplication>
 
-#include "SpacecraftSimulator.h"
+#include "FlightComputer.h"
 
 int main(int argc, char *argv[]){
 
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    engine.loadFromModule("SpacecraftSimulator", "Main");
+    engine.loadFromModule("FlightComputer", "Main");
 
     return app.exec();
 }
---
+++ apps/flight_computer/ui/Main.qml
@@ -0,0 +1,13 @@
+import QtQuick
+import QtQuick.Controls
+
+ApplicationWindow{
+    id: root
+    visible: true
+    title: "Flight Computer"
+
+    Text{
+        anchors.centerIn: parent
+        text: "Flight Computer"
+    }
+}
